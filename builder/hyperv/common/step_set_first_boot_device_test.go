cce43819ba5e2fb23b0a6998a1d2ebc57b631730
William Brooks <williamb@infotronics.com>
2020-02-21 22:50:45 -0600

    created step_set_first_boot_device_test.go

---
+++ builder/hyperv/common/step_set_first_boot_device_test.go
@@ -0,0 +1,15 @@
+package common
+
+import (
+	"testing"
+
+	"github.com/hashicorp/packer/helper/multistep"
+)
+
+func TestStepSetFirstBootDevice_impl(t *testing.T) {
+	var _ multistep.Step = new(StepSetFirstBootDevice)
+}
+
+func TestStepSetFirstBootDevice(t *testing.T) {
+	t.Fatal("Fail IT!")
+}
