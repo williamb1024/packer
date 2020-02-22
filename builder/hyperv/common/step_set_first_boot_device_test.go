fdb562413d5d8e9ca0a0eadfe6483e71e94e1865
William Brooks <williamb@infotronics.com>
2020-02-22 01:43:35 -0600

    more testing

--- builder/hyperv/common/step_set_first_boot_device_test.go
+++ builder/hyperv/common/step_set_first_boot_device_test.go
@@ -13,50 +13,51 @@ type parseBootDeviceIdentifierTest struct {
 	controllerType     string
 	controllerNumber   uint
 	controllerLocation uint
-	failInParse        bool      // true if ParseBootDeviceIdentifier should return an error
-	haltStep           bool      // true if Step.Run should return Halt action
-	shouldCallSet      bool      // true if driver.SetFirstBootDevice should have been called
+	failInParse        bool // true if ParseBootDeviceIdentifier should return an error
+	haltStep           bool // true if Step.Run should return Halt action
+	shouldCallSet      bool // true if driver.SetFirstBootDevice should have been called
+	setDvdProps        bool // true to set DvdDeviceProperties state
 }
 
 var parseIdentifierTests = [...]parseBootDeviceIdentifierTest{
-	{1, "IDE", "IDE", 0, 0, false, false, true},
-	{1, "idE", "IDE", 0, 0, false, false, true},
-	{1, "CD", "CD", 0, 0, false, false, true},
-	{1, "cD", "CD", 0, 0, false, false, true},
-	{1, "DVD", "CD", 0, 0, false, false, true},
-	{1, "Dvd", "CD", 0, 0, false, false, true},
-	{1, "FLOPPY", "FLOPPY", 0, 0, false, false, true},
-	{1, "FloppY", "FLOPPY", 0, 0, false, false, true},
-	{1, "NET", "NET", 0, 0, false, false, true},
-	{1, "net", "NET", 0, 0, false, false, true},
-	{1, "", "", 0, 0, true, false, true},
-	{1, "bad", "", 0, 0, true, true, true},
-	{1, "IDE:0:0", "", 0, 0, true, true, true},
-	{1, "SCSI:0:0", "", 0, 0, true, true, true},
-	{2, "IDE", "", 0, 0, true, true, true},
-	{2, "idE", "", 0, 0, true, true, true},
-	{2, "CD", "CD", 0, 0, false, false, true},
-	{2, "cD", "CD", 0, 0, false, false, true},
-	{2, "DVD", "CD", 0, 0, false, false, true},
-	{2, "Dvd", "CD", 0, 0, false, false, true},
-	{2, "FLOPPY", "", 0, 0, true, true, true},
-	{2, "FloppY", "", 0, 0, true, true, true},
-	{2, "NET", "NET", 0, 0, false, false, true},
-	{2, "net", "NET", 0, 0, false, false, true},
-	{2, "", "", 0, 0, true, false, true},
-	{2, "bad", "", 0, 0, true, false, true},
-	{2, "IDE:0:0", "IDE", 0, 0, false, false, true},
-	{2, "SCSI:0:0", "SCSI", 0, 0, false, false, true},
-	{2, "Ide:0:0", "IDE", 0, 0, false, false, true},
-	{2, "sCsI:0:0", "SCSI", 0, 0, false, false, true},
-	{2, "IDEscsi:0:0", "", 0, 0, true, true, true},
-	{2, "SCSIide:0:0", "", 0, 0, true, true, true},
-	{2, "IDE:0", "", 0, 0, true, true, true},
-	{2, "SCSI:0", "", 0, 0, true, true, true},
-	{2, "IDE:0:a", "", 0, 0, true, true, true},
-	{2, "SCSI:0:a", "", 0, 0, true, true, true},
-	{2, "IDE:0:653", "", 0, 0, true, true, true},
-	{2, "SCSI:-10:0", "", 0, 0, true, true, true},
+	{1, "IDE", "IDE", 0, 0, false, false, true, false},
+	{1, "idE", "IDE", 0, 0, false, false, true, false},
+	{1, "CD", "CD", 0, 0, false, false, true, false},
+	{1, "cD", "CD", 0, 0, false, false, true, false},
+	{1, "DVD", "CD", 0, 0, false, false, true, false},
+	{1, "Dvd", "CD", 0, 0, false, false, true, false},
+	{1, "FLOPPY", "FLOPPY", 0, 0, false, false, true, false},
+	{1, "FloppY", "FLOPPY", 0, 0, false, false, true, false},
+	{1, "NET", "NET", 0, 0, false, false, true, false},
+	{1, "net", "NET", 0, 0, false, false, true, false},
+	{1, "", "", 0, 0, true, false, true, false},
+	{1, "bad", "", 0, 0, true, true, true, false},
+	{1, "IDE:0:0", "", 0, 0, true, true, true, false},
+	{1, "SCSI:0:0", "", 0, 0, true, true, true, false},
+	{2, "IDE", "", 0, 0, true, true, true, false},
+	{2, "idE", "", 0, 0, true, true, true, false},
+	{2, "CD", "CD", 0, 0, false, false, true, false},
+	{2, "cD", "CD", 0, 0, false, false, true, false},
+	{2, "DVD", "CD", 0, 0, false, false, true, false},
+	{2, "Dvd", "CD", 0, 0, false, false, true, false},
+	{2, "FLOPPY", "", 0, 0, true, true, true, false},
+	{2, "FloppY", "", 0, 0, true, true, true, false},
+	{2, "NET", "NET", 0, 0, false, false, true, false},
+	{2, "net", "NET", 0, 0, false, false, true, false},
+	{2, "", "", 0, 0, true, false, true, false},
+	{2, "bad", "", 0, 0, true, false, true, false},
+	{2, "IDE:0:0", "IDE", 0, 0, false, false, true, false},
+	{2, "SCSI:0:0", "SCSI", 0, 0, false, false, true, false},
+	{2, "Ide:0:0", "IDE", 0, 0, false, false, true, false},
+	{2, "sCsI:0:0", "SCSI", 0, 0, false, false, true, false},
+	{2, "IDEscsi:0:0", "", 0, 0, true, true, true, false},
+	{2, "SCSIide:0:0", "", 0, 0, true, true, true, false},
+	{2, "IDE:0", "", 0, 0, true, true, true, false},
+	{2, "SCSI:0", "", 0, 0, true, true, true, false},
+	{2, "IDE:0:a", "", 0, 0, true, true, true, false},
+	{2, "SCSI:0:a", "", 0, 0, true, true, true, false},
+	{2, "IDE:0:653", "", 0, 0, true, true, true, false},
+	{2, "SCSI:-10:0", "", 0, 0, true, true, true, false},
 }
 
 func TestStepSetFirstBootDevice_impl(t *testing.T) {
@@ -104,18 +105,20 @@ func TestStepSetFirstBootDevice(t *testing.T) {
 
 		state := testState(t)
 		driver := state.Get("driver").(*DriverMock)
-	
+
 		// requires the vmName state value
 		vmName := "foo"
 		state.Put("vmName", vmName)
 
 		// pretend that we mounted a DVD somewhere (CD:0:0)
-		var dvdControllerProperties DvdControllerProperties
-		dvdControllerProperties.ControllerNumber = 0
-		dvdControllerProperties.ControllerLocation = 0
-		dvdControllerProperties.Existing = false
-		state.Put("os.dvd.properties", dvdControllerProperties)
-	
+		if identifierTest.stepDvdProps {
+			var dvdControllerProperties DvdControllerProperties
+			dvdControllerProperties.ControllerNumber = 0
+			dvdControllerProperties.ControllerLocation = 0
+			dvdControllerProperties.Existing = false
+			state.Put("os.dvd.properties", dvdControllerProperties)
+		}
+
 		step.Generation = identifierTest.generation
 		step.FirstBootDevice = identifierTest.deviceIdentifier
 
@@ -124,7 +127,7 @@ func TestStepSetFirstBootDevice(t *testing.T) {
 			t.Fatalf("Test %q (gen %v): Bad action: %v", identifierTest.deviceIdentifier, identifierTest.generation, action)
 		}
 
-		if (identifierTest.haltStep) {
+		if identifierTest.haltStep {
 
 			if _, ok := state.GetOk("error"); !ok {
 				t.Fatalf("Test %q (gen %v): Should have error", identifierTest.deviceIdentifier, identifierTest.generation)
@@ -132,7 +135,7 @@ func TestStepSetFirstBootDevice(t *testing.T) {
 
 			// don't perform the remaining checks..
 			continue
-	
+
 		} else {
 
 			if _, ok := state.GetOk("error"); ok {
@@ -150,11 +153,11 @@ func TestStepSetFirstBootDevice(t *testing.T) {
 		}
 
 		if (driver.SetFirstBootDevice_Called) &&
-		   ((driver.SetFirstBootDevice_VmName != vmName) ||
-		    (driver.SetFirstBootDevice_ControllerType != identifierTest.controllerType) ||
-		    (driver.SetFirstBootDevice_ControllerNumber != identifierTest.controllerNumber) ||
-		    (driver.SetFirstBootDevice_ControllerLocation != identifierTest.controllerLocation) ||
-		    (driver.SetFirstBootDevice_Generation != identifierTest.generation)) {
+			((driver.SetFirstBootDevice_VmName != vmName) ||
+				(driver.SetFirstBootDevice_ControllerType != identifierTest.controllerType) ||
+				(driver.SetFirstBootDevice_ControllerNumber != identifierTest.controllerNumber) ||
+				(driver.SetFirstBootDevice_ControllerLocation != identifierTest.controllerLocation) ||
+				(driver.SetFirstBootDevice_Generation != identifierTest.generation)) {
 
 			t.Fatalf("Test %q (gen %v): Called SetFirstBootDevice with unexpected arguments.", identifierTest.deviceIdentifier, identifierTest.generation)
 
